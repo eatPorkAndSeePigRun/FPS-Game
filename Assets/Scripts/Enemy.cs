7c0ff2acecf7de00ec1ea3ea9de87ad988298538
eatPorkAndSeePigRun <811302964@qq.com>
2019-11-16 10:35:39 +0800

    enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    Transform m_transform;
+    Animator m_ani;
+
+    Player m_player;
+    NavMeshAgent m_agent;
+
+    float m_movSpeed = 2.5f;
+    float m_rotSpeed = 5.0f;
+    float m_timer = 2;
+    int m_life = 15;
+
+    //protected EnemySpawn m_spawn;
+
+    void Start()
+    {
+        m_transform = this.transform;
+        m_ani = this.GetComponent<Animator>();
+        m_player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
+        m_agent = GetComponent<NavMeshAgent>();
+        m_agent.speed = m_movSpeed;
+        m_agent.SetDestination(m_player.m_transform.position);
+    }
+
+    void Update()
+    {
+        if (m_player.m_life <= 0)
+            return;
+
+        m_timer -= Time.deltaTime;
+        AnimatorStateInfo stateInfo = m_ani.GetCurrentAnimatorStateInfo(0);
+
+        // 如果处于待机且不是过渡状态
+        if (stateInfo.fullPathHash == Animator.StringToHash("Base Layer.idle") && 
+            !m_ani.IsInTransition(0))
+        {
+            m_ani.SetBool("idle", false);
+            if (m_timer > 0)
+                return;
+           
+            if (Vector3.Distance(m_transform.position, m_player.m_transform.position) < 1.5f)
+            {
+                m_agent.ResetPath();
+                m_ani.SetBool("attack", true);
+            }
+            else
+            {
+                m_timer = 1;
+                m_agent.SetDestination(m_player.m_transform.position);
+                m_ani.SetBool("run", true);
+            }
+        }
+        // 如果处于跑步且不是过渡状态
+        if (stateInfo.fullPathHash == Animator.StringToHash("Base Layer.run") &&
+            !m_ani.IsInTransition(0))
+        {
+            m_ani.SetBool("run", false);
+
+            if (m_timer < 0)
+            {
+                m_agent.SetDestination(m_player.m_transform.position);
+                m_timer = 1;
+            }
+
+            if (Vector3.Distance(m_transform.position, m_player.m_transform.position) < 1.5f)
+            {
+                m_agent.ResetPath();
+                m_ani.SetBool("attack", true);
+            }
+        }
+        // 如果处于攻击且不是过渡状态
+        if (stateInfo.fullPathHash == Animator.StringToHash("Base Layer.attack") &&
+            !m_ani.IsInTransition(0))
+        {
+            RotateTo();
+            m_ani.SetBool("attack", false);
+
+            if (stateInfo.normalizedTime >= 1.0f)
+            {
+                m_ani.SetBool("idle", true);
+                m_timer = 2;
+            }
+        }
+    }
+
+    void RotateTo()
+    {
+        Vector3 targetDir = m_player.m_transform.position - m_transform.position;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir,
+            m_rotSpeed * Time.deltaTime, 0.0f);
+        m_transform.rotation = Quaternion.LookRotation(newDir);
+    }
+}
