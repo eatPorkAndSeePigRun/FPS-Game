e1bc9ffef79a37412b00b16267c3302f1ee75736
eatPorkAndSeePigRun <811302964@qq.com>
2019-11-17 16:11:29 +0800

    mini map

---
+++ Assets/Scripts/MiniCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniCamera : MonoBehaviour
+{
+
+    void Start()
+    {
+        float ratio = (float)Screen.width / Screen.height;
+        this.GetComponent<Camera>().rect = new Rect((1 - 0.2f), (1 - 0.2f * ratio), 0.2f, 0.2f * ratio);
+    }
+}
